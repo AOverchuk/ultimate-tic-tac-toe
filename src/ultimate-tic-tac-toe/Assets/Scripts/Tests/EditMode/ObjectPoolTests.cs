7f90e2187b55386a3a27fa244b631c6057921ff3
Artyom Overchuk <ktulu1317@gmail.com>
2025-12-06 01:51:25 +0600

    [ObjectPoolTests] WhenGetReturnCycle_ThenObjectsAreReusedInLIFO

--- src/ultimate-tic-tac-toe/Assets/Scripts/Tests/EditMode/ObjectPoolTests.cs
+++ src/ultimate-tic-tac-toe/Assets/Scripts/Tests/EditMode/ObjectPoolTests.cs
@@ -27,6 +27,42 @@ namespace Tests.EditMode
             result.Should().BeNull();
         }
 
+        [Test]
+        public void WhenGetReturnCycle_ThenObjectsAreReusedInLIFO()
+        {
+            // Arrange
+            var item1 = new TestClass();
+            var item2 = new TestClass();
+            var item3 = new TestClass();
+
+            // Act & Assert - Return 3 items
+            _pool.Return(item1);
+            _pool.Return(item2);
+            _pool.Return(item3);
+            _pool.GetSize(typeof(TestClass)).Should().Be(3);
+
+            // Act & Assert - Get in LIFO order (Last In First Out)
+            var retrieved3 = _pool.Get<TestClass>();
+            retrieved3.Should().BeSameAs(item3); // last returned
+            _pool.GetSize(typeof(TestClass)).Should().Be(2);
+
+            var retrieved2 = _pool.Get<TestClass>();
+            retrieved2.Should().BeSameAs(item2);
+            _pool.GetSize(typeof(TestClass)).Should().Be(1);
+
+            var retrieved1 = _pool.Get<TestClass>();
+            retrieved1.Should().BeSameAs(item1); // first returned
+            _pool.GetSize(typeof(TestClass)).Should().Be(0);
+
+            // Act & Assert - Return and Get again (reuse)
+            _pool.Return(item2);
+            _pool.GetSize(typeof(TestClass)).Should().Be(1);
+
+            var reusedItem2 = _pool.Get<TestClass>();
+            reusedItem2.Should().BeSameAs(item2); // same instance
+            _pool.GetSize(typeof(TestClass)).Should().Be(0);
+        }
+
         #endregion
 
         #region Test Classes
