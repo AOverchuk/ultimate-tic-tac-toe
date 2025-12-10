2b5c7b2bed20a5cd86adadf01c46800c0befefa7
Artyom Overchuk <ktulu1317@gmail.com>
2025-12-11 02:06:48 +0600

    [BootstrapStateTests] Added tests for BootstrapState

---
+++ src/ultimate-tic-tac-toe/Assets/Scripts/Tests/EditMode/BootstrapStateTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using Runtime.Infrastructure.GameStateMachine;
+using Runtime.Infrastructure.GameStateMachine.States;
+
+namespace Tests.EditMode
+{
+    [TestFixture]
+    public class BootstrapStateTests
+    {
+        private IGameStateMachine _stateMachineMock;
+        private BootstrapState _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _stateMachineMock = Substitute.For<IGameStateMachine>();
+            _sut = new BootstrapState(_stateMachineMock);
+        }
+
+        [Test]
+        public void WhenEnter_ThenTransitionsToLoadMainMenuState()
+        {
+            // Act
+            _sut.Enter();
+
+            // Assert
+            _stateMachineMock.Received(1).Enter<LoadMainMenuState>();
+        }
+
+        [Test]
+        public void WhenExit_ThenCompletesWithoutError()
+        {
+            // Act
+            Action act = () => _sut.Exit();
+
+            // Assert
+            act.Should().NotThrow();
+        }
+    }
+}
